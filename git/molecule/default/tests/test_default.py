aa5f1aba203dfec6e542781880f2c0d01b5911ee
Dustin Specker <DustinSpecker@DustinSpecker.com>
2019-01-05 19:05:31 -0600

    feat(git): add git role

---
+++ git/molecule/default/tests/test_default.py
@@ -0,0 +1,19 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_git_apt_repo_setup(host):
+    assert host.file("/etc/apt/sources.list.d/git.list").exists
+
+
+def test_git_installed(host):
+    assert host.package("git").is_installed
+
+
+def test_gitconfig_copied(host):
+    home_dir = host.user().home
+    assert host.file("{home_dir}/.gitconfig".format(home_dir=home_dir)).exists
