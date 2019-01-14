ec2d4018a7c326228e657eeeeee6e03c7ed2c67f
Dustin Specker <DustinSpecker@DustinSpecker.com>
2019-01-13 18:50:47 -0600

    fix(nvm): test against ubuntu:latest

--- nvm/molecule/default/tests/test_default.py
+++ nvm/molecule/default/tests/test_default.py
@@ -9,10 +9,10 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_nvm_installed(host):
     test_cmd = 'source /home/ubuntu/.nvm/nvm.sh && nvm --version'
 
-    assert host.run_expect([0], test_cmd)
+    assert host.run_expect([0], '/bin/bash -c "{}"'.format(test_cmd))
 
 
 def test_node_installed(host):
     test_cmd = 'source /home/ubuntu/.nvm/nvm.sh && nvm use 10'
 
-    assert host.run_expect([0], test_cmd)
+    assert host.run_expect([0], '/bin/bash -c "{}"'.format(test_cmd))
