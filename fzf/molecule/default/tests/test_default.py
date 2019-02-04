aa4286f2b84af586c2abba97789ac9c829d929ad
Dustin Specker <DustinSpecker@DustinSpecker.com>
2019-02-04 06:59:34 -0600

    feat(fzf/zsh): clone fzf repository
    Clone entire repository so that the Vim plugin will work.
    
    Still downloading bin manually instead of using fzf/install to avoid
    needing Go installed.

--- fzf/molecule/default/tests/test_default.py
+++ fzf/molecule/default/tests/test_default.py
@@ -7,12 +7,12 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_fzf_installed(host):
-    assert host.file('/usr/local/bin/fzf').exists
+    assert host.file('/home/ubuntu/.fzf/bin/fzf').exists
 
 
 def test_fzf_completions_downloaded(host):
-    assert host.file('/home/ubuntu/.fzf/completion.zsh').exists
+    assert host.file('/home/ubuntu/.fzf/shell/completion.zsh').exists
 
 
 def test_fzf_key_bindings_downloaded(host):
-    assert host.file('/home/ubuntu/.fzf/key-bindings.zsh').exists
+    assert host.file('/home/ubuntu/.fzf/shell/key-bindings.zsh').exists
--- fzf/readme.md
+++ fzf/readme.md
@@ -6,6 +6,8 @@ Installs fzf, sets up completions, and key bindings.
 Requirements
 ------------
 
+Git must be installed.
+
 On Mac, it is expected that homebrew has been installed.
 
 Role Variables
