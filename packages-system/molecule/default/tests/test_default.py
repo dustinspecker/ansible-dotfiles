a65ffdbac8a3701b1042b6e6db12719d6af1ac76
Dustin Specker <DustinSpecker@DustinSpecker.com>
2019-09-11 06:52:52 -0500

    refactor(packages-sytem): support testing package versions

--- packages-system/molecule/default/tests/test_default.py
+++ packages-system/molecule/default/tests/test_default.py
@@ -8,10 +8,14 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_packages_are_installed(host):
     packages = [
-        'bat',
+        {'name': 'bat', 'version': '0.9.0'},
         'jq',
         'silversearcher-ag'
     ]
 
     for package in packages:
-        assert host.package(package).is_installed
+        if type(package) is str:
+            assert host.package(package).is_installed
+        else:
+            assert host.package(package['name']).is_installed
+            assert host.package(package['name']).version == package['version']
