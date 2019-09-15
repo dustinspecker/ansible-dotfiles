89875629f94a60da0ce2817c82c7605303e6ebf3
Dustin Specker <DustinSpecker@DustinSpecker.com>
2019-09-15 18:05:11 -0500

    feat(packages-system): update fd to 7.4.0

--- packages-system/molecule/default/tests/test_default.py
+++ packages-system/molecule/default/tests/test_default.py
@@ -6,7 +6,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 BAT_VERSION = '0.12.1'
-FD_VERSION = '7.2.0'
+FD_VERSION = '7.4.0'
 
 
 def test_packages_are_installed(host):
