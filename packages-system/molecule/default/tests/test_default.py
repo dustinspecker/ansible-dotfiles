dd962be48dd694bdac4b76f5d8816bc5f468a79b
Dustin Specker <DustinSpecker@DustinSpecker.com>
2019-09-15 17:44:31 -0500

    refactor(packages-system): move versions to vars

--- packages-system/molecule/default/tests/test_default.py
+++ packages-system/molecule/default/tests/test_default.py
@@ -5,11 +5,14 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+BAT_VERSION = '0.12.1'
+FD_VERSION = '7.2.0'
+
 
 def test_packages_are_installed(host):
     packages = [
-        {'name': 'bat', 'version': '0.12.1'},
-        {'name': 'fd', 'version': '7.2.0'},
+        {'name': 'bat', 'version': BAT_VERSION},
+        {'name': 'fd', 'version': FD_VERSION},
         'jq',
         'silversearcher-ag'
     ]
