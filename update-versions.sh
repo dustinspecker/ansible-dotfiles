1696c918cde03b9a0f2f410904cb5fd55bee18d9
Dustin Specker <DustinSpecker@DustinSpecker.com>
2019-09-15 17:58:54 -0500

    feat(proj): add update-versions script

---
+++ update-versions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v http)" ]; then
+  echo "Error: httpie must be installed" >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v jq)" ]; then
+  echo "Error: jq must be installed" >&2
+  exit 1
+fi
+
+update_version() {
+  local github_org="$1"
+  local repo_name="$2"
+
+  local upper_repo_name
+  upper_repo_name="$(echo "$repo_name" | tr '[:lower:]' '[:upper:]')"
+
+  latest_version=$(http "https://api.github.com/repos/$github_org/$repo_name/releases" | jq -crM 'first(.[]) | .tag_name' | sed 's/^v//')
+
+  sed -Ei.bak "s/(${upper_repo_name}_VERSION: ).*/\1$latest_version/" packages-system/tasks/debian.yml
+  sed -Ei.bak "s/(${upper_repo_name}_VERSION = ').*/\1$latest_version'/" packages-system/molecule/default/tests/test_default.py
+}
+
+update_version "sharkdp" "bat"
+update_version "sharkdp" "fd"
