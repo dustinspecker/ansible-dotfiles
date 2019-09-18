80641da4ccc0ccbc3a9efa5954344fc9127833ad
Dustin Specker <DustinSpecker@DustinSpecker.com>
2019-09-18 18:36:16 -0500

    feat(update-versions): support passing in task/test_file for sed

--- update-versions.sh
+++ update-versions.sh
@@ -13,14 +13,16 @@ fi
 update_version() {
   local github_org="$1"
   local repo_name="$2"
+  local task_file="${3:-packages-system/tasks/debian.yml}"
+  local test_file="${4:-packages-system/molecule/default/tests/test_default.py}"
 
   local upper_repo_name
   upper_repo_name="$(echo "$repo_name" | tr '[:lower:]' '[:upper:]')"
 
   latest_version=$(http "https://api.github.com/repos/$github_org/$repo_name/releases" | jq -crM 'first(.[]) | .tag_name' | sed 's/^v//')
 
-  sed -Ei.bak "s/(${upper_repo_name}_VERSION: ).*/\1$latest_version/g" packages-system/tasks/debian.yml
-  sed -Ei.bak "s/(${upper_repo_name}_VERSION = ').*/\1$latest_version'/g" packages-system/molecule/default/tests/test_default.py
+  sed -Ei.bak "s/(${upper_repo_name}_VERSION: ).*/\1$latest_version/g" "$task_file"
+  sed -Ei.bak "s/(${upper_repo_name}_VERSION = ').*/\1$latest_version'/g" "$test_file"
 }
 
 update_version "sharkdp" "bat"
