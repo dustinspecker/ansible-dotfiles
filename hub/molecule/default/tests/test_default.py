a2acc8aadc06576bf1bf218db40c3ed3edc5b887
Dustin Specker <DustinSpecker@DustinSpecker.com>
2019-10-02 06:08:02 -0500

    refactor(hub): move version to HUB_VERSION var

--- hub/molecule/default/tests/test_default.py
+++ hub/molecule/default/tests/test_default.py
@@ -5,8 +5,10 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+HUB_VERSION = '2.7.0'
+
 
 def test_hub_installed(host):
     test_cmd = 'hub --version | grep hub'
 
-    assert host.check_output(test_cmd) == 'hub version 2.7.0'
+    assert host.check_output(test_cmd) == 'hub version %s' % HUB_VERSION
