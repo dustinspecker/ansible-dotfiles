00951c84cf16d5068daca38e0df06ea9c0a9ca96
Dustin Specker <DustinSpecker@DustinSpecker.com>
2019-10-04 06:47:06 -0500

    test(nvm): verify nvm version

--- nvm/molecule/default/tests/test_default.py
+++ nvm/molecule/default/tests/test_default.py
@@ -5,11 +5,15 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+NVM_VERSION = '0.34.0'
+
 
 def test_nvm_installed(host):
     test_cmd = 'source /home/ubuntu/.nvm/nvm.sh && nvm --version'
 
-    assert host.run_expect([0], '/bin/bash -c "{}"'.format(test_cmd))
+    version = host.check_output('/bin/bash -c "{}"'.format(test_cmd))
+
+    assert version == NVM_VERSION
 
 
 def test_node_installed(host):
