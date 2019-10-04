b06aa42cebbdb6146a034d815a5f8fcaf37d62d9
Dustin Specker <DustinSpecker@DustinSpecker.com>
2019-10-03 19:01:29 -0500

    test(fzf): verify version of bin installed

--- fzf/molecule/default/tests/test_default.py
+++ fzf/molecule/default/tests/test_default.py
@@ -5,15 +5,17 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+FZF_VERSION = '0.18.0'
+
 
 def test_fzf_cloned_and_checked_out_tag(host):
     cmd = 'git -C ~/.fzf describe --exact-match HEAD'
-    FZF_VERSION = '0.18.0'
     assert host.check_output(cmd) == FZF_VERSION
 
 
-def test_fzf_installed(host):
-    assert host.file('/home/ubuntu/.fzf/bin/fzf').exists
+def test_fzf_bin_installed(host):
+    cmd = "/home/ubuntu/.fzf/bin/fzf --version | awk '{ print $1 }'"
+    assert host.check_output(cmd) == FZF_VERSION
 
 
 def test_fzf_completions_downloaded(host):
