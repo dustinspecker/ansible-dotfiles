016f1a8a938c4202ad176f5eb0fcc6a067071014
Dustin Specker <DustinSpecker@DustinSpecker.com>
2019-10-23 06:22:15 -0500

    feat(tmux): upgrade to 2.9a

--- tmux/molecule/default/tests/test_default.py
+++ tmux/molecule/default/tests/test_default.py
@@ -5,7 +5,7 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-TMUX_VERSION = '2.8'
+TMUX_VERSION = '2.9a'
 TPM_VERSION = '3.0.0'
 
 
