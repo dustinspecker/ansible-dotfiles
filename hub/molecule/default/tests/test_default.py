17ee6fdcfd6cc767e03745692655f49a196972cf
Dustin Specker <DustinSpecker@DustinSpecker.com>
2020-12-07 18:07:21 -0600

    feat(hub): update hub to 2.14.2

--- hub/molecule/default/tests/test_default.py
+++ hub/molecule/default/tests/test_default.py
@@ -5,7 +5,7 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-HUB_VERSION = '2.13.0'
+HUB_VERSION = '2.14.2'
 
 
 def test_hub_installed(host):
