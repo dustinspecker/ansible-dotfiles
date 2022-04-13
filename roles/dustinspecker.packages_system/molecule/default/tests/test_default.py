d0c4ef6c87c64e8bbccec8c52eb64f82e9c41e63
Dustin Specker <DustinSpecker@DustinSpecker.com>
2022-04-13 18:10:19 -0500

    feat(packages_system): update fd to 8.3.2

--- roles/dustinspecker.packages_system/molecule/default/tests/test_default.py
+++ roles/dustinspecker.packages_system/molecule/default/tests/test_default.py
@@ -6,7 +6,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 BAT_VERSION = '0.20.0'
-FD_VERSION = '8.3.0'
+FD_VERSION = '8.3.2'
 
 
 def test_packages_are_installed(host):
